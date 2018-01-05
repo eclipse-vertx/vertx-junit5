ad833095c7af99021e023b7d08b31447a25a7b29
Julien Ponge <julien.ponge@gmail.com>
2018-01-05 15:57:57 +0100

    Simplify awaitCompletion
    Fixes #20

--- src/main/java/io/vertx/junit5/VertxTestContext.java
+++ src/main/java/io/vertx/junit5/VertxTestContext.java
@@ -249,7 +249,7 @@ public final class VertxTestContext {
    * @throws InterruptedException when the thread has been interrupted.
    */
   public boolean awaitCompletion(long timeout, TimeUnit unit) throws InterruptedException {
-    return failed() || releaseLatch.await(timeout, unit);
+    return releaseLatch.await(timeout, unit);
   }
 
   // ........................................................................................... //

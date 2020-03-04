c21b9b27e0893d761a55cd63bdde24ce58475911
Julien Ponge <jponge@redhat.com>
2020-03-04 15:37:46 +0100

    WebClient support moved to another project

--- vertx-junit5-web-client/src/main/java/examples/MultipleAnnotationsTest.java
+++
@@ -1,9 +0,0 @@
-package examples;
-
-import io.vertx.junit5.VertxExtension;
-import io.vertx.junit5.web.VertxWebClientExtension;
-import org.junit.jupiter.api.extension.ExtendWith;
-
-@ExtendWith(VertxExtension.class)
-@ExtendWith(VertxWebClientExtension.class)
-public class MultipleAnnotationsTest { }

85a40108f26c22ebeca28eed0298fe228d57622b
Alexander Sinyuskin <sinyushkin@gmail.com>
2025-10-23 18:29:16 +0200

    Rework test to be run programmatically from another test

--- src/test/java/io/vertx/junit5/tests/WaitForContextInAfterEachMethodTest.java
+++ src/test/java/io/vertx/junit5/tests/WaitForContextInAfterEachMethodTest.java
@@ -5,36 +5,38 @@ import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
+@Disabled("Executed only via programmatic launcher from MetaTest")
 @ExtendWith({VertxExtension.class})
 public class WaitForContextInAfterEachMethodTest {
-    static final AtomicBoolean afterTestContextAwaited = new AtomicBoolean(false);
+  static final AtomicBoolean afterTestContextAwaited = new AtomicBoolean(false);
 
-    @Test
-    void test(final VertxTestContext context) {
-        context.failNow(new RuntimeException("Failing test through context"));
+  @Test
+  void test(final VertxTestContext context) {
+    context.failNow(new RuntimeException("Failing test through context"));
+  }
+
+  @AfterAll
+  static void afterAll(final VertxTestContext context) {
+    if (afterTestContextAwaited.get()) {
+      context.completeNow();
+    } else {
+      context.failNow(new RuntimeException("afterTest context was not awaited"));
     }
+  }
 
-    @AfterAll
-    static void afterAll(final VertxTestContext context) {
-      if (afterTestContextAwaited.get()) {
-        context.completeNow();
-      } else {
-        context.failNow(new RuntimeException("afterTest context was not awaited"));
-      }
-    }
+  @AfterEach
+  void afterTest(final Vertx vertx, final VertxTestContext context) {
+    vertx.setTimer(100, id -> {
+      afterTestContextAwaited.set(true);
+      context.completeNow();
+    });
 
-    @AfterEach
-    void afterTest(final Vertx vertx, final VertxTestContext context) {
-        vertx.setTimer(100, id -> {
-            afterTestContextAwaited.set(true);
-            context.completeNow();
-        });
-
-    }
+  }
 
 }
